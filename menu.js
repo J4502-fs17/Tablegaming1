c9f66d20abab670006d098d4aaa25e3d1612a95d
Yasmine Benchaabane <yasminebench@users.noreply.github.com>
2017-11-12 17:20:55 -0600

    added js code

---
+++ menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('#nav').slideToggle(500);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) {
+		$('#nav').removeAttr('style');
+	 }
+	});//end resize
+});
